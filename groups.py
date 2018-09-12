83c0f3e9fb7c2bdcbff13dbe16bdcd513e27b50a
Vlad Shulga <vladyslav.shulha@nure.ua>
2018-09-12 22:39:32 +0300

    asd

--- groups.py
+++ groups.py
@@ -10,16 +10,16 @@ cyrillic = r.data.decode('cp1251')
 DATA = json.loads(r.data.decode('cp1251'))
 
 
-string = "line for debug"
+# string = "line for debug"
 
-url2 = "http://cist.nure.ua/ias/app/tt/P_API_EVEN_JSON?timetable_id=6496579&type_id=1&time_from=1535760000&time_to=1543622400"
-r2 = http.request('GET', url2)
+# url2 = "http://cist.nure.ua/ias/app/tt/P_API_EVEN_JSON?timetable_id=6496579&type_id=1&time_from=1535760000&time_to=1543622400"
+# r2 = http.request('GET', url2)
 
-cyrillic2 = r2.data.decode('cp1251')
+# cyrillic2 = r2.data.decode('cp1251')
 
-DATA2 = json.loads(r2.data.decode('cp1251'))
+# DATA2 = json.loads(r2.data.decode('cp1251'))
 
-string = "line for debug"
+# string = "line for debug"
 
 # def group_name(group_id):
 #     url = "http://cist.nure.ua/ias/app/tt/P_API_EVENTS_GROUP_JSON?p_id_group="
