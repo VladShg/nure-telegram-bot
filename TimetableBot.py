3e6674a2cca945476a19b8ddf758746548e793cd
Vlad Shulga <vladyslav.shulha@nure.ua>
2018-09-12 23:21:40 +0300

    sad

--- TimetableBot.py
+++ TimetableBot.py
@@ -120,7 +120,7 @@ async def timetable(id, num):
         for e in event_day[k]:
             s += '\n'
             # s += "[" + str(start[e['number_pair'] - 1]) + "-" + str(end[e['number_pair'] - 1]) + "] " + "[" + e['auditory'] + "] "
-            s += "[№" + str(e['number_pair']) + "] " + "[" + e['auditory'] + "] "
+            s += str(e['number_pair']) + ") " + e['auditory'] + "- "
             a = subject_full_name(obj, e["subject_id"])
             if short_s == 1:
                 a = subject_short_name(obj, e["subject_id"])
