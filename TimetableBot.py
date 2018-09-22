b5ddf64bd811ba1441e676a1420d721d19b604aa
Vlad Shulga <vladyslav.shulha@nure.ua>
2018-09-22 20:04:59 +0300

    ads

--- TimetableBot.py
+++ TimetableBot.py
@@ -653,7 +653,7 @@ async def process_timetable_custom_command(msg: types.Message):
         now = datetime.datetime.now() + datetime.timedelta(hours=3)
         if now.day == datetime.datetime.strptime(todayKey, "%d.%m.%Y").day:
             n = event_day[len(event_day) - 1]['number_pair']
-            if now > TIME['end'[n-1]]:
+            if now > TIME['end'][n-1]]:
                 timer = "Пары закончились, можно чилить"
             else:
                 timer += "\n\n"
