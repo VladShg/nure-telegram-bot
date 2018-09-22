cb732aef2ba8aafaec184a1ca4b13d97fae4037b
Vlad Shulga <vladyslav.shulha@nure.ua>
2018-09-22 21:58:04 +0300

    фыв

--- TimetableBot.py
+++ TimetableBot.py
@@ -362,7 +362,7 @@ async def process_callback_timetable_swift(call: types.CallbackQuery):
             print("if 1")
             if shift > 0:
                     start = TIME['start'][event_day[0]['number_pair'] -1]
-                    h = (24 - s_start.hour - 3) + start.hour
+                    h = (24 - s_start.hour - 4) + start.hour
                     m = (60 - s_start.minute) + start.minute
                     timer += str(datetime.timedelta(days=shift, hours=h, minutes=m))
                     print("if 2")
