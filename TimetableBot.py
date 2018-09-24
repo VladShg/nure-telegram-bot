10c45aba6c8ab8e369b107a75eef69164655285e
Vlad Shulga <vladyslav.shulha@nure.ua>
2018-09-24 13:22:21 +0300

    qwd

--- TimetableBot.py
+++ TimetableBot.py
@@ -767,38 +767,30 @@ async def alarm_command(msg: types.Message):
                     await bot.send_message(MY_ID, str(e) + " " + u[1])
                     time.sleep(0.5)
 
+@dp.message_handler(func=lambda message: message.from_user.id == MY_ID, regexp=r"\A(/send)", content_types=ContentType.PHOTO)
+async def process_send_image_command(message: types.Message):
+    try:
+        id = int(message['photo'][0]['caption'][5:16])
+        s = message['caption'][16:]
+        print(id)
+        print(s)
+        s += "\nОтветить: /reply [text]"
+        await bot.send_photo(chat_id=id, caption=s, photo=message['photo'][0]['file_id'])
+    except Exception as e:
+        await bot.send_message(MY_ID, str(e))
+
 @dp.message_handler(regexp=r"\A(/send)", func=lambda message: message.from_user.id == MY_ID)
-async def process_sendm_command(message: types.Message):
+async def process_send_message_command(message: types.Message):
     id = int(message.text[5:16])
     msg = message.text[16:]
-    print(id)
-    print(msg)
     msg += "\nОтветить: /reply [text]"
     try:
         await bot.send_message(id, msg)
     except Exception as e:
         await bot.send_message(MY_ID, str(e))
 
-
-@dp.message_handler(func=lambda message: message.from_user.id == MY_ID and bool(re.match("\A(/send)", message.text) == True))
-async def process_send_message_command(message: types.Message):
-    print(message.from_user.id == MY_ID)
-    print(bool(re.match("\A(/send)", message.text) == True))
-    try:
-        print("inside sendm")
-        id = int(message['caption'][5:15])
-        s = message['caption'][16:]
-        print(id)
-        print(s)
-        s += "\nОтветить: /reply [text]"
-        await bot.send_message(chat_id=id, text=s)
-    except Exception as e:
-        await bot.send_message(MY_ID, str(e))
-
 @dp.message_handler(content_types=ContentType.PHOTO, func=lambda message: bool(re.match(r"\A(/send)", message.photo[0]['caption']) == True))
-async def process_send_image_command(message: types.Message):
-    if message.from_user.id != MY_ID:
-        return
+async def process_send_image2_command(message: types.Message):
     try:
         print("inside sendp")
         id = int(message['caption'][5:15])
