b62be3ff8374fa7591ade52e1396870596993cba
Vlad Shulga <vladyslav.shulha@nure.ua>
2018-09-24 07:25:13 +0300

    wqd

--- TimetableBot.py
+++ TimetableBot.py
@@ -707,7 +707,7 @@ async def process_timetable_custom_command(msg: types.Message):
                         break
                     if now < TIME['start'][e['number_pair']-1]:
                         time = TIME['start'][e['number_pair']-1] - now
-                        timer += "До начала пары: " + str(time)
+                        timer += "До начала пары: " + str(time)[:7]
                         break
 
         s = todayKey + " " + weekday(datetime.datetime.now().weekday())  + "\n"
